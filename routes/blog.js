e317db2dc87daaffb5d1d779bfbb479dd8b9bdf8
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-08-02 23:18:33 +0545

    blog input and store

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,25 @@
 const express = require("express");
+const multer = require("multer");
+const Blog = require("../models/blog");
 
+const path = require("path");
 const router = express.Router();
+
+//storage
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve("./public/uploads"));
+  },
+  filename: function (req, file, cb) {
+    const fileName = `${Date.now()}-${file.originalname}`;
+    cb(null, fileName);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
 
 router.get("/add-new", (req, res) => {
   return res.render("addBlog", {
@@ -8,9 +27,15 @@ router.get("/add-new", (req, res) => {
   });
 });
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  return res.redirect("/");
+router.post("/", upload.single("coverImage"), async (req, res) => {
+  const { title, body } = await req.body;
+  Blog.create({
+    title,
+    body,
+    coverImageURL: `uploads/${req.file.filename}`,
+    createdBy: req.user._id,
+  });
+  return res.redirect(`/blog/${blog._id}`);
 });
 
 module.exports = router;
